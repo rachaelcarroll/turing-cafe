b9ede07e18fe4ade9cfca65de471efec75606ff1
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-21 13:43:31 -0400

    Write fetch call to POST new reso

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getReservations = () => {
     return fetch('http://localhost:3001/api/v1/reservations')
       .then(response => response.json())
   }
+
+export const addReservation = (reservation) => {
+  fetch('http://localhost:3001/api/v1/reservations', {
+      method: 'POST',
+      body: JSON.stringify({
+        "name": reservation.name,
+        "date": reservation.date,
+        "time": reservation.time,
+        "number": reservation.number,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+}
