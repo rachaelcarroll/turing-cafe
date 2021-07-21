608e99291f0eaa353d3ab9871ac450bb49e0f6d5
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-21 12:21:44 -0400

    Build render for Form and create submit reservation handle change func

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -11,4 +11,57 @@ class Form extends Component {
             number: ''
         }
     }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitReservation = (event, props) => {
+        event.preventDefault()
+        let newReso = {
+            id: Date.now(),
+            ...this.state
+        }
+    } 
+
+
+    render() {
+        return (
+            <form className='resoForm'>
+                <input 
+                    type='text'
+                    name='name'
+                    placeholder='Full Name'
+                    value={this.state.name}
+                    onChange={(event) => this.handleChange(event)}
+                />
+                <input 
+                    type='text'
+                    name='date'
+                    placeholder='Date'
+                    value={this.state.date}
+                    onChange={(event) => this.handleChange(event)}
+                />
+                <input 
+                    type='text'
+                    name='time'
+                    placeholder='Time'
+                    value={this.state.time}
+                    onChange={(event) => this.handleChange(event)}
+                />
+                <input 
+                    type='number'
+                    name='number'
+                    placeholder='# of Guests'
+                    value={this.state.number}
+                    onChange={(event) => this.handleChange(event)}
+                />
+                <button className='submitReso' onClick={(event) => this.submitReservation(event)}>
+                    Make Reservation
+                </button>
+            </form>
+        )
+    }
 }
