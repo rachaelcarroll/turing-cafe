40feea1f7de5f749b789e6f86e11526a6bd5ffd8
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-21 11:51:19 -0400

    Update state in App component to hold reservation array

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getReservations } from '../../apiCalls'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount = () => {
+    getReservations()
+    .then(reservations => this.setState({ reservations: reservations })
+    .catch(() => this.setState({ error: 'Oops, something went wrong, please try again!'}))
+  }
+
+  
   render() {
     return (
       <div className="App">
